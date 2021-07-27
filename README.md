7fbee8ea69b22f66ab2cf9620e9b95cf057fe76a
etahinnocent360 <etahinno360@gmail.com>
2021-07-27 10:43:20 +0100

    local

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # ATM demo app
 
-# Deployment procedures 
+# Deployment procedures
 
 # 1) Creating netlify account if you do not have one
 
-# 2)push app to github 
+# 2)push app to github
 
 # 3) Connect netlify with github choose a repository, and select the branch to be deployed.
-Branch is set to  auto deploy in order for netlify to automatically deploy any changes made on the branch.
+  Branch is set to  auto deploy in order for netlify to automatically deploy any changes made on the branch.
 
 # 4) once deployed set a custom domain for the site
 
-# 5)secure site with https. 
-Once this is done you can now save and preview site
- 
+# 5)secure site with https.
+  Once this is done you can now save and preview site
+
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var app = angular.module('plunker', []);
 
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', function ($scope) {
 
-  var cleanAccount = function() {
+  var cleanAccount = function () {
     var account = {
       name: 'Etah innocent',
       startingBalace: 100.00,
@@ -10,8 +10,8 @@ app.controller('MainCtrl', function($scope) {
     }
     return account;
   };
-  
-   var cleanTransaction = function() {
+
+  var cleanTransaction = function () {
     var transaction = {
       type: 'Withdraw',
       amount: 0.00,
@@ -21,17 +21,16 @@ app.controller('MainCtrl', function($scope) {
     return transaction;
   };
 
-  var transactions = [
-    ];
+  var transactions = [];
 
- 
+
 
   $scope.transaction = cleanTransaction();
   $scope.account = cleanAccount();
 
   $scope.transactionList = transactions;
 
-  $scope.saveTransaction = function() {
+  $scope.saveTransaction = function () {
     var amount = parseFloat($scope.transaction.amount);
     var num = parseFloat($scope.account.runningBalance);
     var answer = 0;
@@ -50,7 +49,7 @@ app.controller('MainCtrl', function($scope) {
 });
 
 
-app.directive('moneywarn', function() {
+app.directive('moneywarn', function () {
   var staticWarningLevel = .2;
 
   return {
@@ -58,13 +57,13 @@ app.directive('moneywarn', function() {
     scope: {
       val: '=moneywarn'
     },
-    link: function(scope, element, attrs) {
-      scope.$watch('val', function(newValue) {
+    link: function (scope, element, attrs) {
+      scope.$watch('val', function (newValue) {
         var startBalance = parseInt(attrs.startbalance);
         var warningLevel = startBalance * staticWarningLevel;
         if (newValue === warningLevel) {
           element.addClass('alert-warning');
-           element.removeClass('alert-danger');
+          element.removeClass('alert-danger');
         } else if (newValue < warningLevel) {
           element.addClass('alert-danger');
         } else {
@@ -77,3 +76,71 @@ app.directive('moneywarn', function() {
   }
 
 });
+
+document.querySelectorAll('.key')
+
+window.pass = 1234;
+window.redirectURL = './index.html';
+
+
+$(document).ready(function() {
+	start();
+});
+
+
+function start(){
+	window.tries = 0;
+	
+	$(".key").click(function(){
+		var n = $(this).html();
+		$('.screen').append( n );
+		window.tries++;
+		updateFlasher();
+		validate();
+	});
+}
+
+function updateFlasher(){
+	if (window.tries <=3){
+		var dis = window.tries * 55;
+		$('.flasher').css({
+			'left' : dis + 'px'
+		});
+	}
+	else{
+		$('.flasher').css({
+			'left' : '20px',
+			'display' : 'none'
+		});
+	}
+}
+
+function validate(){
+	if (window.tries >= 4){
+		checkWin();
+		$('.screen').html('');
+		window.tries = 0;
+		$('.flasher').css({
+			'display' : 'block'
+		});
+	}
+	else{
+		
+	}
+}
+
+function checkWin(){
+	var w = $('.screen').html();
+	if (w == window.pass){
+		$('.success').show().delay(5000).queue(function(n) {
+			$('.success').hide(); n();
+		});
+		var u = window.redirectURL;
+		$(location).attr('href', u );
+	}
+	else{
+		$('.error').show().delay(1000).queue(function(n) {
+			$('.error').hide(); n();
+		});
+	}
+}
